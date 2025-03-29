d320607dee5b3c9be9188aee27325498aca3a134
EliboyevElbek <coderpython107@gmail.com>
2025-03-30 04:09:56 +0500

    minor changes

--- config/urls.py
+++ config/urls.py
@@ -19,6 +19,8 @@ from django.urls import path, include
 from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
+from django.conf import settings
+from django.conf.urls.static import static
 
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
@@ -48,5 +50,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/v1/', include('app_lessons.urls')),
     path('api/v1/', include('accounts.urls'))
-]
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
