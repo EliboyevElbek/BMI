3888445d4dc128c015429dfdbba12231d433ea18
EliboyevElbek <coderpython107@gmail.com>
2025-04-22 12:59:32 +0500

    frontend

--- app_lessons/permissions.py
+++ app_lessons/permissions.py
@@ -1,12 +1,12 @@
-# from rest_framework.permissions import BasePermission, SAFE_METHODS
-#
-#
-# class IsOwnerOfComment(BasePermission):
-#     def has_object_permission(self, request, view, obj):
-#         return obj.from_user == request.user
-#
-# class CustomCategoryPermission(BasePermission):
-#     def has_permission(self, request, view):
-#         if request.method in SAFE_METHODS:
-#             return True
-#         return request.user and request.user.is_staff
+from rest_framework.permissions import BasePermission, SAFE_METHODS
+
+
+class IsOwnerOfComment(BasePermission):
+    def has_object_permission(self, request, view, obj):
+        return obj.from_user == request.user
+
+class CustomCategoryPermission(BasePermission):
+    def has_permission(self, request, view):
+        if request.method in SAFE_METHODS:
+            return True
+        return request.user and request.user.is_staff
--- config/settings.py
+++ config/settings.py
@@ -41,14 +41,17 @@ INSTALLED_APPS = [
     'rest_framework',
     # My created apps
     'rest_framework_simplejwt',
+    'corsheaders',
     'django_filters',
     'app_lessons',
     'accounts',
 ]
 
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -137,6 +140,7 @@ MEDIA_ROOT = str(BASE_DIR.joinpath('media'))
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = 'accounts.CustomUser'
+CORS_ALLOW_ALL_ORIGINS = True
 
 REST_FRAMEWORK = {
     'DEFAULT_FILTER_BACKENDS': [
